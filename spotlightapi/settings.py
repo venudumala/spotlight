ced7ce16c936042f2b9f34a9f931a76e654a66c6
anuraagpathak-ppltech <anuraag.pathak@peopletech.com>
2022-12-26 18:12:22 +0000

    changes in Server

--- spotlightapi/settings.py
+++ spotlightapi/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-mh__#y)t&g^^bznssxv4&z5*$(mnpvyz5)j-v&row@9a9@(h0y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
