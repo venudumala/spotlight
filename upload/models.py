f9ead64604b9997ffc4f8b113b515c613aa91a99
anuraagpathak-ppltech <anuraag.pathak@peopletech.com>
2022-12-26 22:41:55 +0530

    Rename filename to filepath

--- upload/models.py
+++ upload/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Upload(models.Model):
     file =models.FileField(upload_to='uploadfiles/',null=True)
     file_name =models.CharField(max_length=100)
-    file_path =models.CharField(max_length=200)
+    file_type =models.CharField(max_length=200)
 
     def __str__(self):
         return self.file
--- upload/serializers.py
+++ upload/serializers.py
@@ -4,4 +4,4 @@ from .models import Upload
 class UploadSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=Upload
-        fields=('id','url','file','file_name','file_path')
+        fields=('id','url','file','file_name','file_type')
