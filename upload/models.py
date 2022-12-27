b4c2bcf95e039907d7ce78171cf695c8022f7869
anuraagpathak-ppltech <anuraag.pathak@peopletech.com>
2022-12-27 13:30:49 +0530

    changed in

--- upload/models.py
+++ upload/models.py
@@ -1,9 +1,11 @@
+import datetime
 from django.db import models
 
 class Upload(models.Model):
     file =models.FileField(upload_to='uploadfiles/',null=True)
     file_name =models.CharField(max_length=100)
     file_type =models.CharField(max_length=200)
+    created_at=models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.file
--- upload/serializers.py
+++ upload/serializers.py
@@ -4,4 +4,4 @@ from .models import Upload
 class UploadSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=Upload
-        fields=('id','url','file','file_name','file_type')
+        fields=('id','url','file','file_name','file_type','created_at')
