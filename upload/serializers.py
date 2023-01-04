92873046f7880f1e8087efb8f05c77f25ad2ecd6
anuraagpathak-ppltech <anuraag.pathak@peopletech.com>
2023-01-04 15:16:37 +0530

    changes in update logic

--- upload/serializers.py
+++ upload/serializers.py
@@ -2,19 +2,6 @@ from rest_framework import serializers
 from .models import Upload
 from django.db import models
 
-# class UploadSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model=Upload
-#         fields=('id','record_id','url','file','file_name','file_type','upoload_layer','dump_layer','transformation_layer','records_inserted','created_at')
-
-# class UploadLayerSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=Upload
-#         fields=('id','record_id','dump_layer','transformation_layer','records_inserted')
-
-#         def update(self, instance, validated_data):
-#             return super().update(instance, validated_data)
-
 class UploadSerializer(serializers.Serializer):
     id=serializers.IntegerField(read_only=True)
     record_id=serializers.IntegerField(read_only=True)
@@ -32,15 +19,8 @@ class UploadSerializer(serializers.Serializer):
         return Upload.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.record_id=validated_data.get('record_id',instance.record_id)
-        instance.file=validated_data.get('file',instance.file)
-        instance.file_name=validated_data.get('file_name',instance.file_name)
-        instance.file_type=validated_data.get('file_type',instance.file_type)
-        instance.upoload_layer=validated_data.get('upoload_layer',instance.upoload_layer)
         instance.dump_layer=validated_data.get('dump_layer',instance.dump_layer)
         instance.transformation_layer=validated_data.get('transformation_layer',instance.transformation_layer)
-        instance.records_inserted=validated_data.get('records_inserted',instance.records_inserted)
-        instance.valid_file_path=validated_data.get('valid_file_path',instance.valid_file_path)
         instance.updated_at=validated_data.get('updated_at',instance.updated_at)
         instance.save()
         return instance
