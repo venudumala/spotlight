97893a44fd2ae84ad955e2040838aefbfe4fbdbe
anuraagpathak-ppltech <anuraag.pathak@peopletech.com>
2023-02-14 12:43:12 +0530

    http://20.81.21.75:8000

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -4,9 +4,9 @@ from user_app.api.views import logout_view, registration_view
 from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView,)
 
 urlpatterns = [
-    path('login/', obtain_auth_token,name='login'),
+    # path('login/', obtain_auth_token,name='login'),
     path('register/', registration_view,name='register'),
-    path('logout/', logout_view,name='logout'),
+    # path('logout/', logout_view,name='logout'),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
