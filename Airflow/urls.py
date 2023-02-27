48c19d5de600d1c2dabfa975b02fd9021e94acde
maheshkolipaka <maheshkolipaka992@gmail.com>
2023-02-27 17:07:59 +0530

    Added Sourcestatus and fail_msg APIs

--- Airflow/urls.py
+++ Airflow/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('sourcetest/',views.sourceTestMssql.as_view(), name='sourcetest'),
+    path('sourceStatus',views.SourceStatus.as_view(), name='sourceStatus'),
+    path('fail_Msg',views.SourceFailMsg.as_view(), name='fail_Msg'),
     path('getSourceId/',views.getSourceID.as_view(), name='sourceid'),
     path('getConnId/',views.getConnId.as_view(), name='connid'),
     path('getTableList/',views.getTablesList.as_view(), name='tables'),
