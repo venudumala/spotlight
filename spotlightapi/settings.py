2c7c0f05b942fce4693281aee212b19519dbf6b4
maheshkolipaka <maheshkolipaka992@gmail.com>
2023-03-02 11:42:00 +0530

    Sent email after sucessfull registration added

--- spotlightapi/settings.py
+++ spotlightapi/settings.py
@@ -178,3 +178,10 @@ AWS_STORAGE_BUCKET_NAME='spotlightus'
 
 AWS_QUERYSTRING_AUTH=False
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'spotlight.poc.alerts@gmail.com'
+EMAIL_HOST_PASSWORD = 'ygmpxvfecvgzpogu'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
